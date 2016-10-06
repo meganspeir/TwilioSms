30a3c74bb146d10c5a0ebdd69aa6fcf44b1f8f84
meganspeir <2zamfvjrymwd@meganspeir.com>
2016-10-05 17:13:21 -0700

    TwilioSms project template

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TwilioSms' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TwilioSms
+
+  pod 'Alamofire', '~> 4.0.1'
+
+end
---
+++ TwilioSms/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TwilioSms
+//
+//  Created by Megan Speir on 9/23/16.
+//  Copyright © 2016 Twilio. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var phoneNumberField: UITextField!
+    @IBOutlet var messageField: UITextField!
+    
+    @IBAction func sendData(sender: AnyObject) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
